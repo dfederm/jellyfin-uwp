adbf59a0f2fab9c1f85c08019d8e5d8a2f766afc
athulrajts <athulrajts@gmail.com>
2025-03-26 21:35:05 +0530

    - hide sections that has 0 items - reduce duplicated xaml

---
+++ Jellyfin/Common/EmptyCollectionToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using System.Collections.Generic;
+
+namespace Jellyfin.Common;
+
+internal sealed class EmptyCollectionToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value is not IEnumerable<object> enumerable
+            ? Visibility.Collapsed
+            : enumerable.Count() == 0 ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new InvalidOperationException($"{nameof(EmptyCollectionToVisibilityConverter)} cannot be used in a two-way binding");
+    }
+}
